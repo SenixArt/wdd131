9a7d696120960495de712a43c54d13faf06b9d83
SenixArt <105945599+SenixArt@users.noreply.github.com>
2026-09-02 21:12:46 -0400

    Delete week01 directory

--- week01/scripts/getdates.js
+++
@@ -1,8 +0,0 @@
-// Dynamic current year for footer copyright
-const currentYearSpan = document.getElementById("currentyear");
-const today = new Date();
-currentYearSpan.textContent = today.getFullYear();
-
-// Dynamic last modified date for footer
-const lastModifiedParagraph = document.getElementById("lastModified");
-lastModifiedParagraph.textContent = `Last Modification: ${document.lastModified}`;
